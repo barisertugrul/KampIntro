85111c8d89d23e668c3caf63f1afc9d11eafa4a3
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-02-07 05:20:42 +0300

    7 Şubat Ders sonrası değişikliğiProje dosyası ekle.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //Naming convention _ Metotların ilk harfi büyük
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler! {0} sepete eklendi.",urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string Aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. {0} sepete eklendi",urunAdi);
+        }
+    }
+}
